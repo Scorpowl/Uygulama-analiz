1d98d7f77a39c3da3eb10abcf61b89d1c4019c0b
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-10 17:08:35 +0300

    Uygulama-analiz

--- uygulama.py
+++ uygulama.py
@@ -19,8 +19,6 @@ def load():
     data = pd.read_csv("titanic.csv")
     return data
 
-df = load()
-
 def outlier_th(dataframe, col_name, q1=0.25, q3=0.75):
     quartile1 = dataframe[col_name].quantile(q1)
     quartile3 = dataframe[col_name].quantile(q3)
@@ -68,6 +66,17 @@ def grab_outliers(dataframe,col_name,index=False):
         outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
         print(outlier_index)
 
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
 def remove_outliers(dataframe,col_name):
     low, up = outlier_th(dataframe,col_name)
     df_without = dataframe[~((dataframe[col_name] < low) | (dataframe[col_name] > up))]
@@ -81,6 +90,11 @@ def label_encoder(dataframe,binary_col):
 def one_hot_encoder(dataframe, categorical_cols, drop_first = False):
     dataframe = pd.get_dummies(dataframe, columns=categorical_cols,drop_first=drop_first)
     return dataframe
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_th(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
 
 def cat_summary(dataframe, col_name, plot = False):
     print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
@@ -108,3 +122,88 @@ def rare_encoder(dataframe, rare_perc):
         temp_df[var] = np.where(temp_df[var].isin(rare_labels), "Rare", temp_df[var])
 
     return temp_df
+
+df = load()
+df.columns = [col.upper() for col in df.columns]
+print(df)
+
+# Cabin bool
+df["NEW_CABIN_BOOL"] = df["CABIN"].notnull().astype('int')
+# Name count
+df["NEW_NAME_COUNT"] = df["NAME"].str.len()
+# name word count
+df["NEW_NAME_WORD_COUNT"] = df["NAME"].apply(lambda x: len(str(x).split(" ")))
+# name dr
+df["NEW_NAME_DR"] = df["NAME"].apply(lambda x: len([x for x in x.split() if x.startswith("Dr")]))
+# name title
+df['NEW_TITLE'] = df.NAME.str.extract(' ([A-Za-z]+)\.', expand=False)
+# family size
+df["NEW_FAMILY_SIZE"] = df["SIBSP"] + df["PARCH"] + 1
+# age_pclass
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+# is alone
+df.loc[((df['SIBSP'] + df['PARCH']) > 0), "NEW_IS_ALONE"] = "NO"
+df.loc[((df['SIBSP'] + df['PARCH']) == 0), "NEW_IS_ALONE"] = "YES"
+# age level
+df.loc[(df['AGE'] < 18), 'NEW_AGE_CAT'] = 'young'
+df.loc[(df['AGE'] >= 18) & (df['AGE'] < 56), 'NEW_AGE_CAT'] = 'mature'
+df.loc[(df['AGE'] >= 56), 'NEW_AGE_CAT'] = 'senior'
+# sex x age
+df.loc[(df['SEX'] == 'male') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngmale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturemale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniormale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngfemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturefemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniorfemale'
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+num_cols = [col for col in num_cols if col not in "PASSENGERID"]
+
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+df.drop("CABIN", inplace=True, axis=1)
+
+remove_cols = ["TICKET", "NAME"]
+df.drop(remove_cols, inplace=True, axis=1)
+
+df["AGE"] = df["AGE"].fillna(df.groupby("NEW_TITLE")["AGE"].transform("median"))
+
+# age level
+df.loc[(df['AGE'] < 18), 'NEW_AGE_CAT'] = 'young'
+df.loc[(df['AGE'] >= 18) & (df['AGE'] < 56), 'NEW_AGE_CAT'] = 'mature'
+df.loc[(df['AGE'] >= 56), 'NEW_AGE_CAT'] = 'senior'
+# sex x age
+df.loc[(df['SEX'] == 'male') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngmale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturemale'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniormale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'youngfemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] > 21) & (df['AGE'] < 50), 'NEW_SEX_CAT'] = 'maturefemale'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] >= 50), 'NEW_SEX_CAT'] = 'seniorfemale'
+
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+
+df = df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis=0)
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+               and df[col].nunique() == 2]
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+rare_analyser(df, "SURVIVED", cat_cols)
+
+
+df = rare_encoder(df, 0.01)
+
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+df = one_hot_encoder(df, ohe_cols)
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+print(df)
